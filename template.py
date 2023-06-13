c184ba613f5abb149a58eb21eb7d4041adb7b2e5
Ubuntu <azureuser@mlops.e1vcjtzfurkufcuve1bmqz3m2c.bx.internal.cloudapp.net>
2023-06-13 17:39:31 +0000

    requirements added

--- template.py
+++ template.py
@@ -21,8 +21,8 @@ list_of_files = [
     "params.yaml",
     "requirement.txt",
     "setup.py",
-    "research/trials.ipynb"
-    
+    "research/trials.ipynb",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files :
