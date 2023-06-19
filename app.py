d85a7f950a4c599f48d77b758faee7483e2e5eb0
Ubuntu <azureuser@mlops.e1vcjtzfurkufcuve1bmqz3m2c.bx.internal.cloudapp.net>
2023-06-19 12:33:52 +0000

    docker added

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ if __name__ == "__main__":
     clApp = ClientApp()
     # app.run(host='0.0.0.0', port=8080) #local host
     # app.run(host='0.0.0.0', port=8080) #for AWS
-    app.run(host='0.0.0.0', port=5000) #for AZURE
+    app.run(host='0.0.0.0', port=80) #for AZURE
